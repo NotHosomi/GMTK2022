7976a5a5172f3a58a502a30ffaafd38375206780
Unknown <unknown@example.com>
2022-07-17 19:31:01 +0100

    Menu Transitions

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
